1af2d98a88e179cf64b5a446da93fd030abe7569
Svetlana Novikova <svetlnovikova@gmail.com>
2023-01-05 14:33:58 +0100

    actions.yml

---
+++ build.sh
@@ -0,0 +1,4 @@
+set -e
+/opt/builder/bin/idea.sh helpbuilderinspect -source-dir . -product $PRODUCT -output-dir artifacts/ || true
+echo "Test existing of $ARTIFACT artifact"
+test -e artifacts/$ARTIFACT
