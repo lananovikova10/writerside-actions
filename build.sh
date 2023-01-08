8e41babf8aa101329f1bb369a4c2c4b729b037bd
Lana <svetlnovikova@gmail.com>
2023-01-08 19:48:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 set -e
-/opt/builder/bin/idea.sh helpbuilderinspect -source-dir . -product $PRODUCT -output-dir artifacts/ || true
+/opt/builder/bin/idea.sh helpbuilderinspect -source-dir . --runner github -product $PRODUCT -output-dir artifacts/ || true
 echo "Test existing of $ARTIFACT artifact"
 test -e artifacts/$ARTIFACT
